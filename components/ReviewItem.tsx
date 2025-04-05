4d3e8fb98fd2afb0d350e47ff88d99b06eb9c4a5
PinHao Chen <pinhaocrypto@gmail.com>
2025-04-06 01:23:11 +0800

    Translate reward eligibility text from Chinese to English

--- components/ReviewItem.tsx
+++ components/ReviewItem.tsx
@@ -120,7 +120,7 @@ export default function ReviewItem({ review }: ReviewItemProps) {
                 <svg className="w-3.5 h-3.5 mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                   <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                 </svg>
-                獎勵資格
+                Reward Eligible
               </div>
             )}
           </div>
@@ -136,7 +136,7 @@ export default function ReviewItem({ review }: ReviewItemProps) {
             <svg className="w-4 h-4 mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
               <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2h-1V9a1 1 0 00-1-1z" clipRule="evenodd" />
             </svg>
-            此評論將有機會獲得店家獎勵
+            This review is eligible for rewards
           </div>
         )}
         
